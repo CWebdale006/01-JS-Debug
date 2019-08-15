6240d7e03f9a1cbd92490b371ab4dd88e3489062
connor <cwebdale006@west-mec.org>
2019-08-15 15:27:56 -0700

    working on photos
    worked with nodes and src attributes

--- 02-DocumentObjectModel/photos.js
+++ 02-DocumentObjectModel/photos.js
@@ -2,8 +2,8 @@
 
  *    Photo gallery
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author: Connor J Webdale 
+ *    Date: 8/15/19 
 
  *    Filename: photos.js
  */
@@ -12,6 +12,30 @@
 
 /* global variables */
 var photoOrder = [1,2,3,4,5];
+
+// variable to control figure count 
+var figureCount = 3;  
+
+// provides filename values for the src attributes of the three img elements 
+function populateFigures() {
+   var filename; 
+   var currentFig; 
+
+   if (figureCount === 3) {
+      for (var i = 1; i < 4;i++) {
+         filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+         currentFig = document.getElementsByTagName("img")[i-1]; 
+         currentFig.src = filename; 
+         
+      }
+   } else {
+      for (var i = 0; i<5; i++) {
+         filename = "images/IMG_0" + photoOrder[i] + "sm.jpg"; 
+         currentFig = document.getElementsByTagName("img")[i];
+         currentFig.src = filename;
+      }
+   }
+}
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
@@ -37,9 +61,103 @@ function leftArrow() {
    }
 }
 
+function previewFive() {
+   // creating a new node 
+   var lastFigure = document.createElement("figure");
+   // adding properties to the new element 
+   lastFigure.id = "fig5"; 
+   lastFigure.style.zIndex = "5"; 
+   lastFigure.style.position = "absolute"; 
+   lastFigure.style.right = "45px"; 
+   lastFigure.style.top = "67px"; 
+
+   // creating a new node 
+   var lastImage = document.createElement("img"); 
+   // adding attributes to this element 
+   lastImage.width = "240"; 
+   lastImage.height = "135"; 
+
+   // references the first article element 
+   var articleElem = document.getElementsByTagName("article")[0]; 
+   
+   // attaches the new img element to the new figure element 
+   lastFigure.appendChild(lastImage); 
+
+   // attches the document fragment to the article 
+   // articleElem.appendChild(lastFigure); 
+   articleElem.insertBefore(lastFigure, document.getElementById("rightarrow")); 
+
+   var firstFigure = lastFigure.cloneNode(true); 
+   firstFigure.id = "fig1"; 
+   firstFigure.style.right = ""; 
+   firstFigure.style.left = "45px"; 
+
+   // articleElem.appendChild(firstFigure); 
+   articleElem.insertBefore(firstFigure, document.getElementById("fig2")); 
+
+   // gives the 2 new elements src images 
+   document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+   document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+
+   // resets the figurecount variable 
+   figureCount = 5; 
+
+   // references the button 
+   var numberButton = document.querySelector("#fiveButton"); 
+   numberButton.innerHTML = "Show fewer images"; 
+
+   // changes the event listeners to remove images 
+   if (numberButton.addEventListener) {
+      numberButton.removeEventListener("click", previewFive, false); 
+      numberButton.addEventListener("click, previewThree, false"); 
+   }
+   else if (numberButton.attachEvent) {
+      numberButton.detachEvent("onclick", previewFive); 
+      numberButton.attachEvent("onclick", previewThree); 
+   }
+}
+
 /* open center figure in separate window */
 function zoomFig() {
    
+}
+
+
+function createEventListeners() {
+   // references the left arrow button 
+   var leftarrow = document.getElementById("leftarrow");
+   if (leftarrow.addEventListener) {
+      leftarrow.addEventListener("click", leftArrow, false); 
+   } else if (leftarrow.attachEvent) {
+      leftarrow.attachEvent("onclick", leftArrow); 
+   }
+
+   // references the right arrow button 
+   var rightarrow = document.getElementById("rightarrow");
+   if (rightarrow.addEventListener) {
+      rightarrow.addEventListener("click", rightArrow, false); 
+   } else if (rightarrow.attachEvent) {
+      rightarrow.attachEvent("onclick", rightArrow); 
+   }
+
+   // references the second element of an array of the img element 
+   var mainFig = document.getElementsByTagName("img")[1];
+
+   if (mainFig.addEventListener) {
+      mainFig.addEventListener("click", zoomFig, false); 
+   }
+   else if (mainFig.attachEvent) {
+      mainFig.attachEvent("onclick", zoomFig); 
+   }
+
+   // event handlers for the "show more images" button
+   var showAllButton = document.querySelector("#fiveButton");
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click", previewFive, false); 
+   }
+   else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick", previewFive); 
+   }
 }
 
 /* create event listeners and populate image elements */
